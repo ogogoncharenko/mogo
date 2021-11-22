3472bba02c01c97b41beb9b521680bcb3c8ddcfc
ogogoncharenko <goncharenko.oleksandr@gmail.com>
2021-11-22 19:16:37 +0200

    22.11.2021, adding ccs and  js code for animations, buttons

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 let navtoggle = document.querySelector('.nav-toggle');
 let navActive = document.querySelector('.nav_active');
-let windowSize = window.matchMedia("(min-width: 770px)")
+let windowSize = window.matchMedia("(min-width: 770px)");
+let headerLine = document.querySelector('.header');
+let headerInner = document.querySelector('.header__inner');
+let showScroll = document.getElementById('showScroll');
+let sticky = headerInner.scrollTop;
+let accordionButton = document.querySelector('.accordion__header:after');
+console.log(accordionButton);
+
 
 navtoggle.addEventListener('click', function() {
     navtoggle.classList.add('active');
@@ -13,16 +20,35 @@ navtoggle.addEventListener('click', function() {
         navActive.style.display = 'none';
         console.log(navActive.style.display);
         navtoggle.classList.remove('active');
-}})
+    }
+
+})
 
 function NavStyleRemover(windowSize) {
     console.log(windowSize);
-    if (windowSize.matches) { // Если медиа запрос совпадает;
+    if (windowSize.matches) { // If the media request matches
         navActive.style.display = "";
-    } else {
-        console.log('remove');
-    }
+    } else {}
 }
 
 NavStyleRemover(windowSize);
-windowSize.addListener(NavStyleRemover);
+windowSize.addEventListener('change', NavStyleRemover);
+
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    if (window.scrollY > sticky) {
+        headerInner.classList.add("header_fixed-large");
+        headerLine.classList.add("header_line");
+    } else {
+        headerInner.classList.remove("header_fixed-large");
+        headerLine.classList.remove("header_line");
+    }
+}
+
+accordionButton.addEventListener('click', accordionButtonClicked);
+
+function accordionButtonClicked(value) {
+        console.log(value);
+}
